309c4b3ac9b3a4aba481463d34c81a310009f450
Caelest1s <jefferson.ucb02@gmail.com>
2024-02-08 10:56:07 -0300

    User Role Model
    Create entity User and Role

---
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,75 @@
+package com.devsuperior.demo.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    // attribute
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    // constructor
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    // getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((authority == null) ? 0 : authority.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Role other = (Role) obj;
+
+        // if (authority == null) {
+        //     if (other.authority != null)
+        //         return false;
+        // } else if (!authority.equals(other.authority))
+        //     return false;
+        // return true;
+
+        return Objects.equals(authority, other.authority);
+    }
+
+    
+}

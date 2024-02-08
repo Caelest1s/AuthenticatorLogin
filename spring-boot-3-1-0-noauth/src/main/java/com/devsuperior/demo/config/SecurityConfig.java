20181191903c12ecc81917ed7abc5cab62d3046c
Caelest1s <jefferson.ucb02@gmail.com>
2024-02-08 11:53:19 -0300

    BCrypt Password

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,11 +3,21 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+// Definir componentes na forma de metódos
 @Configuration
 public class SecurityConfig {
     
+    // o @Bean faz transformar o objeto em componente que poderá ser injetado em outros lugares
+    // PasswordEncoder é um genérico do BCryptPasswordEncoder
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // desabilita a proteção contra csrf que é um ataque quando grava dados na seção. Gera vulnerabilidade para acessar as informações de forma indevida

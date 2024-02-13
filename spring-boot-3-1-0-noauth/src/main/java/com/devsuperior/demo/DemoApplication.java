63668aefa8abccd7fea2e99fbe498ae921d6d8fc
Caelest1s <jefferson.ucb02@gmail.com>
2024-02-12 23:49:53 -0300

    Authorization server, Resource server, not tested

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -6,20 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// @Autowired
-	// private PasswordEncoder passwordEncoder;
-
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	// @Override
-	// public void run(String... args) throws Exception {
-	// 	System.out.println("Encode = " + passwordEncoder.encode("123456"));
-
-	// 	boolean result = passwordEncoder.matches("1234567", "$2a$10$eZ06MwljmlZQoSCjFPp8auOgjglRHOzvA6EMVzvEVMFPg6VZoGepi");
-	// 	System.out.println("Resultado = " + result);
-	// }
-
 
 }
--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+/*
+ 
 package com.devsuperior.demo.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -10,12 +12,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-// Definir componentes na forma de metódos
+
 @Configuration
 public class SecurityConfig {
     
-    // o @Bean faz transformar o objeto em componente que poderá ser injetado em outros lugares
-    // PasswordEncoder é um genérico do BCryptPasswordEncoder
+    
+    
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
@@ -34,13 +36,13 @@ public class SecurityConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // desabilita a proteção contra csrf que é um ataque quando grava dados na seção. Gera vulnerabilidade para acessar as informações de forma indevida
-        // API REST não guarda state em seção, então não precisa preocupar com isto
+        
         http.csrf(csrf -> csrf.disable());
-        // configurar as permissões para os meus endpoints, para as minhas requisições
+        
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 
     
 }
+*/
